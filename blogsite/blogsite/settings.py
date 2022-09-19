24b9f0718de8285e0eebb044357e108a13277504
s-harmanitin <nssh4707@gmail.com>
2022-09-19 22:08:06 +0530

    itsns

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yfyvvmvvi*(!#x!(evcb33g04#5v@a86qs56)y9wy!!hc1vuiq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
